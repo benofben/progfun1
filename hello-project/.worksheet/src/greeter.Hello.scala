7c9dfcd916f84c364c31779bf767bcee6c53f174
benofben <ben.lackey@outlook.com>
2016-06-10 18:18:08 -0700

    ye olde reset

--- hello-project/.worksheet/src/greeter.Hello.scala
+++
@@ -1,5 +0,0 @@
-package greeter
-
-object Hello extends App{;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(69); 
-  println("Hello, world.")}
-}
