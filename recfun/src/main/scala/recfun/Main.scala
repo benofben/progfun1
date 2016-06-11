b91f27bc9dc0fe95f2f6092ccee386c0b4402493
benofben <ben.lackey@outlook.com>
2016-06-11 00:01:36 -0700

    seems to work.  Need to figure out why it doesn't like my style

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -2,26 +2,66 @@ package recfun
 
 object Main {
   def main(args: Array[String]) {
+    println(countChange(1,List(1)))
+    println(countChange(1,List(1,2)))
+    println(countChange(1,List(2,1)))
+    println(countChange(2,List(2,1)))
+    println(countChange(3,List(2,1)))
+    println(countChange(4,List(1,2)))
+
+    /*
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
-    }
+    }*/ 
   }
 
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if(c==0 || c==r)
+        1
+      else
+        pascal(c-1,r-1)+pascal(c,r-1)
+    }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      if(balanceInt(chars, 0)!=0)
+        false
+      else
+        true
+    }
+    
+    def balanceInt(chars: List[Char], leftParenCount: Int): Int = {
+      if(chars.isEmpty)
+        leftParenCount
+      else if(chars.head != '(' && chars.head != ')')
+        balanceInt(chars.tail, leftParenCount)
+      else if(chars.head == '(')
+        balanceInt(chars.tail, leftParenCount+1)
+      else if(leftParenCount>0 && chars.head==')')
+        balanceInt(chars.tail, leftParenCount-1)
+      else
+        -1
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
-  }
+    def countChange(money: Int, coins: List[Int]): Int = {
+      if(money==0 || coins.isEmpty)
+        0
+      else if(money-coins.head==0)
+        1+countChange(money, coins.tail)
+      else if(money-coins.head<0)
+        countChange(money, coins.tail)
+      else
+        countChange(money-coins.head, coins)+countChange(money, coins.tail)
+    }
+}
