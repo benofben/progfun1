074923025f531b4a85392f689e4d3db02038d5e5
benofben <ben.lackey@outlook.com>
2016-06-10 17:47:07 -0700

    oh the pain.  I didn't miss eclipse.

---
+++ hello-project/.worksheet/src/greeter.Hello.scala
@@ -0,0 +1,5 @@
+package greeter
+
+object Hello extends App{;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(69); 
+  println("Hello, world.")}
+}
